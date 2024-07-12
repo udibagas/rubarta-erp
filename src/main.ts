a9aa0d3761df5c163eaeb6bb4e95c3159e7f409f
Bagas Udi Sahsangka <udibagas@gmail.com>
2024-07-12 15:18:53 +0700

    use helmet

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { BadRequestException, ValidationPipe } from '@nestjs/common';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(helmet());
   app.useGlobalPipes(
     new ValidationPipe({
       enableDebugMessages: true,
