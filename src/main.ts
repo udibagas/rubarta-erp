8a2f0f1c0411f877ce9f8c34793aca2a2d1e5c62
Bagas Udi Sahsangka <udibagas@gmail.com>
2024-07-12 15:11:20 +0700

    use ValidationPipe

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,35 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { BadRequestException, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      enableDebugMessages: true,
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      stopAtFirstError: true,
+      validationError: {
+        target: false,
+        value: false,
+      },
+      exceptionFactory: (errors) => {
+        const error = {};
+
+        errors.forEach((e) => {
+          error[e.property] = Object.values(e.constraints)[0];
+        });
+
+        throw new BadRequestException({
+          statusCode: 400,
+          message: 'Bad Request',
+          error: error,
+        });
+      },
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
