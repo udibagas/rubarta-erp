abecabfd9c48c228e887c713f22a69a508410e78
Bagas Udi Sahsangka <udibagas@gmail.com>
2024-07-13 06:30:17 +0700

    implemnet swagger

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,26 +1,22 @@
-import {
-  Body,
-  Controller,
-  Get,
-  Post,
-  Request,
-  UseGuards,
-} from '@nestjs/common';
+import { Body, Controller, Get, HttpCode, Post, Request } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { AuthGuard } from './auth.guard';
 import { Public } from './public.decorator';
+import { LoginDto } from './login.dto';
+import { ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Auth')
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Public()
   @Post()
-  signIn(@Body() { email, password }): Promise<{ token: string }> {
-    return this.authService.signIn(email, password);
+  @HttpCode(200)
+  signIn(@Body() credential: LoginDto): Promise<{ token: string }> {
+    return this.authService.signIn(credential.email, credential.password);
   }
 
-  @Get('me')
+  @Get()
   me(@Request() req) {
     return req.user;
   }
