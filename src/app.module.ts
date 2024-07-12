3326dba783b4efa2ef361d578aa4cb0f3c386b73
Bagas Udi Sahsangka <udibagas@gmail.com>
2024-07-13 06:30:43 +0700

    register banksmodule

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { UsersModule } from './users/users.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
+import { BanksModule } from './banks/banks.module';
 
 @Module({
   imports: [
@@ -10,6 +11,7 @@ import { AuthModule } from './auth/auth.module';
     UsersModule,
     PrismaModule,
     AuthModule,
+    BanksModule,
   ],
   controllers: [],
   providers: [],
