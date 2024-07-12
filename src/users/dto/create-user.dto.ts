facccb47c04bb320f5a91068458c312f37dd7c74
Bagas Udi Sahsangka <udibagas@gmail.com>
2024-07-12 15:19:24 +0700

    apply validation

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,8 +1,20 @@
 import { Role } from '@prisma/client';
+import { IsEmail, IsNotEmpty, IsOptional, Length } from 'class-validator';
 
 export class CreateUserDto {
+  @IsNotEmpty({ message: 'Name is required' })
   name: string;
+
+  @IsEmail({}, { message: 'Invalid email' })
   email: string;
+
+  @IsOptional()
   role: Role;
+
+  @IsNotEmpty({ message: 'Password is required' })
+  @Length(8, undefined, { message: 'Minimum password 8 is characters' })
   password: string;
+
+  @IsOptional()
+  bankAccount: string;
 }
