babbf8cac2d8c3f26236acc6859ad64c8a396a37
Bagas Udi Sahsangka <udibagas@gmail.com>
2024-07-14 20:16:25 +0700

    add example

--- src/auth/login.dto.ts
+++ src/auth/login.dto.ts
@@ -2,11 +2,11 @@ import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsNotEmpty } from 'class-validator';
 
 export class LoginDto {
-  @ApiProperty()
+  @ApiProperty({ example: 'admin@mail.com' })
   @IsEmail()
   email: string;
 
-  @ApiProperty()
+  @ApiProperty({ example: 'admin123' })
   @IsNotEmpty()
   password: string;
 }
