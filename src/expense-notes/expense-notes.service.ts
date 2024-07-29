6484a58b27c50a19d90ad33e6c2b7ea89b9f7ddc
Bagas Udi Sahsangka <udibagas@gmail.com>
2024-07-29 18:58:54 +0700

    also delete attachment

--- src/expense-notes/expense-notes.service.ts
+++ src/expense-notes/expense-notes.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 import { ExpenseNoteDto } from './dto/expense-note.dto';
+import * as fs from 'node:fs/promises';
+import { Prisma } from '@prisma/client';
 
 @Injectable()
 export class ExpenseNotesService {
@@ -29,8 +31,14 @@ export class ExpenseNotesService {
     });
   }
 
-  remove(id: number) {
-    // TODO: hapus attachments
+  async remove(id: number) {
+    const data = await this.findOne(id);
+
+    if (data.attachment) {
+      const attachment = data.attachment as { filePath: string };
+      await fs.unlink(attachment.filePath);
+    }
+
     return this.prisma.expenseNote.delete({ where: { id } });
   }
 }
