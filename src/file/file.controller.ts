8776f6f9e97d637eaf022ab542b50323d154a8a3
Bagas Udi Sahsangka <udibagas@gmail.com>
2024-07-29 16:58:56 +0700

    do it proper way

--- src/file/file.controller.ts
+++ src/file/file.controller.ts
@@ -1,47 +1,67 @@
 import {
   Controller,
   Delete,
+  FileTypeValidator,
   Get,
+  MaxFileSizeValidator,
+  ParseFilePipe,
   Post,
   Query,
   UploadedFile,
   UseInterceptors,
 } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
+import * as multer from 'multer';
 import * as fs from 'node:fs/promises';
 
 @Controller('api/file')
 export class FileController {
   @Post()
-  @UseInterceptors(FileInterceptor('file'))
-  async uploadFile(@UploadedFile() file: Express.Multer.File) {
-    const { originalname: fileName, size: fileSize, mimetype: fileType } = file;
+  @UseInterceptors(
+    FileInterceptor('file', {
+      storage: multer.diskStorage({
+        async destination(req, file, callback) {
+          const [day, month, year] = new Date()
+            .toLocaleString('id-ID', {
+              year: 'numeric',
+              month: '2-digit',
+              day: '2-digit',
+            })
+            .split('/');
 
-    const [day, month, year] = new Date()
-      .toLocaleString('id-ID', {
-        year: 'numeric',
-        month: '2-digit',
-        day: '2-digit',
-      })
-      .split('/');
-
-    const timestamp = Date.now();
-    const path = [year, month, day].join('/');
-    await fs.mkdir(`./uploads/${path}`, { recursive: true });
-    const filePath = `./uploads/${path}/${timestamp}-${fileName}`;
-    await fs.writeFile(filePath, file.buffer);
-    return { fileName, filePath, fileSize, fileType };
+          const path = [year, month, day].join('/');
+          await fs.mkdir(`./uploads/${path}`, { recursive: true });
+          callback(null, `./uploads/${path}`);
+        },
+        filename(req, file, callback) {
+          callback(null, `${Date.now()}-${file.originalname}`);
+        },
+      }),
+    }),
+  )
+  async uploadFile(
+    @UploadedFile(
+      new ParseFilePipe({
+        validators: [
+          new MaxFileSizeValidator({ maxSize: 2000000 }),
+          new FileTypeValidator({ fileType: 'image' }),
+        ],
+      }),
+    )
+    file: Express.Multer.File,
+  ) {
+    const {
+      originalname: fileName,
+      size: fileSize,
+      mimetype: fileType,
+      path: filePath,
+    } = file;
+    return { fileName, fileSize, fileType, filePath };
   }
 
   @Delete()
   async removeFile(@Query('path') path: string) {
     await fs.unlink(path);
     return { message: 'File has beed deleted' };
-  }
-
-  @Get()
-  async getBase64StringByPath(@Query('path') path: string) {
-    const buffer = await fs.readFile(path);
-    return buffer.toString('base64');
   }
 }
