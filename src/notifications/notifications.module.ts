12fbf6d70bbce47271abcfba0024c5230784370a
Bagas Udi Sahsangka <udibagas@gmail.com>
2024-07-31 07:04:58 +0700

    make global

--- src/notifications/notifications.module.ts
+++ src/notifications/notifications.module.ts
@@ -1,7 +1,8 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 import { NotificationsService } from './notifications.service';
 import { NotificationsController } from './notifications.controller';
 
+@Global()
 @Module({
   providers: [NotificationsService],
   controllers: [NotificationsController],
