c5d4b0663afc0951128e52cf8b5997052f961591
Bagas Udi Sahsangka <udibagas@gmail.com>
2024-08-02 10:51:41 +0700

    notif ga bisa pakai html tag

--- src/payment-authorizations/payment-authorizations.service.ts
+++ src/payment-authorizations/payment-authorizations.service.ts
@@ -407,7 +407,7 @@ export class PaymentAuthorizationsService {
     this.notification.notify({
       userId: approval.userId,
       title: `Permintaan ${action}: ${data.number}`,
-      message: `Anda mendapatkan permintaan ${action} untuk <b>Nota Kuasa Pembayaran</b> dengan nomor <b>${data.number}</b>.`,
+      message: `Anda mendapatkan permintaan ${action} untuk Nota Kuasa Pembayaran dengan nomor ${data.number}.`,
       redirectUrl: `https://erp.rubarta.co.id/payment-authorizations?number=${data.number}`,
     });
   }
