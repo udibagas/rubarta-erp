714f4781abea3fabf8768b64570a4b0933738e88
udibagas <udibagas@gmail.com>
2025-12-29 12:42:59 +0700

    refactor moveFile endpoint to use memory storage and handle file writing

--- src/file/file.controller.ts
+++ src/file/file.controller.ts
@@ -14,6 +14,7 @@ import * as multer from 'multer';
 import * as fs from 'node:fs/promises';
 import { FileDto } from './fle.dto';
 import { FileService } from './file.service';
+import { Public } from 'src/auth/public.decorator';
 
 @Controller('api/file')
 export class FileController {
@@ -69,26 +70,10 @@ export class FileController {
   }
 
   @Post('move-file')
+  @Public()
   @UseInterceptors(
     FileInterceptor('file', {
-      storage: multer.diskStorage({
-        async destination(req, file, callback) {
-          const attachment = await this.fileService.getPathByName(
-            file.originalname,
-          );
-
-          if (!attachment) {
-            return callback(new Error('File not found in database'), null);
-          }
-
-          const path = attachment.filePath.split('/').slice(0, -1).join('/');
-          await fs.mkdir(`./${path}`, { recursive: true });
-          callback(null, `./${path}`);
-        },
-        filename(req, file, callback) {
-          callback(null, file.originalname);
-        },
-      }),
+      storage: multer.memoryStorage(),
     }),
   )
   async moveFile(
@@ -102,12 +87,23 @@ export class FileController {
     )
     file: Express.Multer.File,
   ): Promise<FileDto> {
-    const {
-      originalname: fileName,
-      size: fileSize,
-      mimetype: fileType,
-      path: filePath,
-    } = file;
-    return { fileName, fileSize, fileType, filePath };
+    const attachment = await this.fileService.getPathByName(file.originalname);
+
+    if (!attachment) {
+      throw new Error('File not found in database');
+    }
+
+    const destPath = attachment.filePath.split('/').slice(0, -1).join('/');
+    await fs.mkdir(`./${destPath}`, { recursive: true });
+
+    const fullPath = `./${attachment.filePath}`;
+    await fs.writeFile(fullPath, file.buffer);
+
+    return {
+      fileName: file.originalname,
+      fileSize: file.size,
+      fileType: file.mimetype,
+      filePath: fullPath,
+    };
   }
 }
