44446ac35b508a65f0cde19f12fc0a891adccf38
udibagas <udibagas@gmail.com>
2026-07-04 08:39:29 +0700

    fix(interactions): simplify isPaginated condition in findAll method

--- src/interactions/interactions.controller.ts
+++ src/interactions/interactions.controller.ts
@@ -64,7 +64,7 @@ export class InteractionsController {
       opportunityId,
       contactId,
       type,
-      isPaginated: isPaginated === true || isPaginated === 'true',
+      isPaginated: isPaginated === true,
       page,
       limit,
     });
